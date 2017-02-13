d6ad6b601397022b6197d60b1de47b33c30e366b
Nicolas Antoniazzi <nicolas@codingame.com>
2017-02-13 15:42:53 +0100

    Initial import

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("HotDroid");
+        System.out.println("HotDroid");
+        System.out.println("HotDroid");
+        System.out.println("HotDroid");
+        System.out.println("Buzz");
+    }
+}
